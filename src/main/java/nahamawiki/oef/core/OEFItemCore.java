1737d29d6da032e396b7c2b68f0a49cd28ad6430
Tom Kate <tomo23katakata@gmail.com>
2015-09-20 17:39:02 +0900

    真・匠式宝剣シリーズ
    アイテムだけとりあえず追加

--- src/main/java/nahamawiki/oef/core/OEFItemCore.java
+++ src/main/java/nahamawiki/oef/core/OEFItemCore.java
@@ -14,6 +14,10 @@ public class OEFItemCore {
 
 	public static Item EEMater;
 	public static Item EESword;
+	public static Item EESword_RED;
+	public static Item EESword_BLUE;
+	public static Item EESword_GREEN;
+	public static Item EESword_WHITE;
 
 	/** アイテムを追加・登録する */
 	public static void registerItems() {
@@ -23,9 +27,29 @@ public class OEFItemCore {
 		GameRegistry.registerItem(EEMater, EEMater.getUnlocalizedName());
 
 		EESword = new ItemEESword(EES_0)
-		.setUnlocalizedName("EESword")
-		.setTextureName(OEFCore.DOMEINNAME + "EESword");
-GameRegistry.registerItem(EESword, EESword.getUnlocalizedName());
+			.setUnlocalizedName("EESword")
+			.setTextureName(OEFCore.DOMEINNAME + "EESword");
+		GameRegistry.registerItem(EESword, EESword.getUnlocalizedName());
+		
+		EESword_RED = new ItemEESword(EES_0)
+			.setUnlocalizedName("EESword_RED")
+			.setTextureName(OEFCore.DOMEINNAME + "EESword_RED");
+		GameRegistry.registerItem(EESword_RED, EESword_RED.getUnlocalizedName());
+		
+		EESword_BLUE = new ItemEESword(EES_0)
+			.setUnlocalizedName("EESword_BLUE")
+			.setTextureName(OEFCore.DOMEINNAME + "EESword_BLUE");
+		GameRegistry.registerItem(EESword_BLUE, EESword_BLUE.getUnlocalizedName());
+	
+		EESword_GREEN = new ItemEESword(EES_0)
+			.setUnlocalizedName("EESword_GREEN")
+			.setTextureName(OEFCore.DOMEINNAME + "EESword_GREEN");
+		GameRegistry.registerItem(EESword_GREEN, EESword_GREEN.getUnlocalizedName());
+		
+		EESword_WHITE = new ItemEESword(EES_0)
+			.setUnlocalizedName("EESword_WHITE")
+			.setTextureName(OEFCore.DOMEINNAME + "EESword_WHITE");
+		GameRegistry.registerItem(EESword_WHITE, EESword_WHITE.getUnlocalizedName());
 	}
 
 }
